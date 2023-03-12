7cfc7965a2611bb3bccdc8b8060d8853b615f736
LikerFeed <vanya18845@gmail.com>
2023-03-12 18:26:08 +0200

    added PrefixToInfix function

--- implementation.go
+++ implementation.go
@@ -1,9 +1,48 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // TODO: document this function.
 // PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+
+func PrefixToInfix(input string) (string, error) {
+	var stack []string
+	const operators = "+-*/^"
+	const error = "invalid input expression"
+	if len(input) == 0 {
+		return "", fmt.Errorf(error)
+	}
+
+	var inputs = strings.Split(input, " ")
+	symbol, expression1, expression2 := "", "", ""
+
+	for i := len(inputs) - 1; i >= 0; i-- {
+		symbol = inputs[i]
+
+		if !strings.Contains(operators, symbol) {
+			stack = append(stack, symbol)
+		} else {
+			if len(stack) < 2 {
+				return "", fmt.Errorf(error)
+			}
+
+			expression1, stack = Pop(stack)
+			expression2, stack = Pop(stack)
+
+			var new = Join(expression1, expression2, symbol)
+			stack = append(stack, new)
+		}
+	}
+
+	var res = ""
+	res, stack = Pop(stack)
+
+	if len(stack) > 0 {
+		return "", fmt.Errorf(error)
+	}
+
+	return res, nil
 }

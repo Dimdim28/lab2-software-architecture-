1f8d050b58ca12e0ae142ba2ef62792a37c3228d
LikerFeed <vanya18845@gmail.com>
2023-05-22 22:23:09 +0300

    ReadAll 10 points fix

--- handler_test.go
+++ handler_test.go
@@ -2,34 +2,35 @@ package lab2
 
 import (
 	"bytes"
-	. "gopkg.in/check.v1"
 	"strings"
+
+	. "gopkg.in/check.v1"
 )
 
 func (s *MySuite) TestComputeHandler(c *C) {
 	b := bytes.NewBuffer(make([]byte, 0))
 
 	handler := ComputeHandler{
-		Input:  strings.NewReader("- 6 2"),
+		Input:  strings.NewReader("- 1006 2"),
 		Output: b,
 	}
 	err := handler.Compute()
 
 	c.Assert(err, Equals, nil)
-	c.Assert(b.String(), Equals, "6 - 2")
+	c.Assert(b.String(), Equals, "1006 - 2")
 }
 
 func (s *MySuite) TestComputeHandlerHard(c *C) {
 	b := bytes.NewBuffer(make([]byte, 0))
 
 	handler := ComputeHandler{
-		Input:  strings.NewReader("/ 5 * + 9 8 3"),
+		Input:  strings.NewReader("/ 5 * + 1009 1008 3"),
 		Output: b,
 	}
 	err := handler.Compute()
 
 	c.Assert(err, Equals, nil)
-	c.Assert(b.String(), Equals, "5 / ((9 + 8) * 3)")
+	c.Assert(b.String(), Equals, "5 / ((1009 + 1008) * 3)")
 }
 
 func (s *MySuite) TestComputeHandlerError(c *C) {
